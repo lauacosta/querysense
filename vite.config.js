b858b48f093f1f8c755b6d56029174ef1f64c94c
Lautaro Acosta Quintana <acostaquintanalautaro@gmail.com>
2024-11-07 10:35:30 -0300

    js, css and vite config

---
+++ vite.config.js
@@ -0,0 +1,46 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import autoprefixer from "autoprefixer";
+import cssnano from "cssnano";
+
+export default defineConfig({
+	build: {
+		outDir: "dist",
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, "assets/main.js"),
+				styles: resolve(__dirname, "assets/styles.css"),
+			},
+			output: {
+				entryFileNames: "[name].js",
+				chunkFileNames: "[name].js",
+				assetFileNames: "[name].[ext]",
+			},
+		},
+		minify: "terser",
+		terserOptions: {
+			compress: {
+				drop_console: true,
+				drop_debugger: true,
+				passes: 3,
+			},
+		},
+		cssMinify: true,
+	},
+	css: {
+		postcss: {
+			plugins: [
+				autoprefixer(),
+				cssnano({
+					preset: [
+						"default",
+						{
+							discardComments: { removeAll: true },
+							normalizeWhitespace: true,
+						},
+					],
+				}),
+			],
+		},
+	},
+});
